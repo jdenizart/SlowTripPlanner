f636711ee284002e5254fe8756a7560eb59c8d37
meiry11 <meirycisse11@gmail.com>
2022-07-11 21:31:05 +0200

    suppression du dossier entity et repository dur la branche dev

--- back/slowSpring/src/main/java/aston/slowspring/repository/BaliseRepo.java
+++ back/slowSpring/src/main/java/aston/slowspring/repository/BaliseRepo.java
@@ -2,16 +2,6 @@ package aston.slowspring.repository;
 
 import aston.slowspring.entity.Balise;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface BaliseRepo extends JpaRepository<Balise,Long> {
-
-    List<Balise> findByNomBalise(String nombalise);
-
-    List<Balise> findBydDateTime(String datetime);
-
-
 }
--- back/slowSpring/src/main/java/aston/slowspring/repository/PointRepo.java
+++ back/slowSpring/src/main/java/aston/slowspring/repository/PointRepo.java
@@ -1,14 +1,7 @@
 package aston.slowspring.repository;
 
-import aston.slowspring.entity.Parcours;
 import aston.slowspring.entity.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PointRepo extends JpaRepository<Point,Long> {
-    List<Point> findByLongitude(String longitude);
-
-    List<Point> findByLatitude(String latitude);
-
 }
