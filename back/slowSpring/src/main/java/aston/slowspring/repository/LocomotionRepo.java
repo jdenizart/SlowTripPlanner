1f473dc4e63228ee63248ba86ef289da84466408
meiry11 <meirycisse11@gmail.com>
2022-07-11 22:00:29 +0200

    suppression du dossier entity et repository sur la branche features-entity

--- back/slowSpring/src/main/java/aston/slowspring/repository/LocomotionRepo.java
+++
@@ -1,7 +0,0 @@
-package aston.slowspring.repository;
-
-import aston.slowspring.entity.Locomotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocomotionRepo extends JpaRepository<Locomotion,Long> {
-}
--- back/slowSpring/src/main/java/aston/slowspring/repository/ParcoursRepo.java
+++
@@ -1,7 +0,0 @@
-package aston.slowspring.repository;
-
-import aston.slowspring.entity.Parcours;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParcoursRepo extends JpaRepository<Parcours,Long> {
-}
