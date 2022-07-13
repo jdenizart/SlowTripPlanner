c50dadc58c52d16b6866204d9bf430f4ec9e1146
Adrien <amh.marceau@gmail.com>
2022-07-13 16:27:23 +0200

    front-parcours: feature: ajout des composants nécessaires au calendrier

--- front/slowAngular/src/app/app.module.ts
+++ front/slowAngular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParcoursComponent } from './pages/parcours/parcours.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { DateComponent } from './pages/parcours/components/date/date.component';
+import { BaliseComponent } from './pages/parcours/components/balise/balise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParcoursComponent,
+    CalendarComponent,
+    DateComponent,
+    BaliseComponent
   ],
   imports: [
     BrowserModule,
---
+++ front/slowAngular/src/app/pages/parcours/components/calendar/components/date/components/balise/balise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-balise',
+  templateUrl: './balise.component.html',
+  styleUrls: ['./balise.component.css']
+})
+export class BaliseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
