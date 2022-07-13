2d9acb2c35e856846b5e5578671306f6fcd02b0d
meiry11 <meirycisse11@gmail.com>
2022-07-13 14:28:55 +0200

    creation du controller balise et locomotion

--- back/slowSpring/src/main/java/aston/slowspring/services/LocomotionService.java
+++ back/slowSpring/src/main/java/aston/slowspring/services/LocomotionService.java
@@ -1,7 +1,30 @@
 package aston.slowspring.services;
 
+import aston.slowspring.entity.Balise;
+import aston.slowspring.entity.Locomotion;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LocomotionService {
+    public List<Locomotion> findAll() {
+        return null;
+    }
+
+    public List<Locomotion> findByLibeleloco(String libeleloco) {
+        return null;
+    }
+
+    public Locomotion create(Locomotion newLocomotion) {
+        return newLocomotion;
+    }
+
+    public Locomotion update(Locomotion locomotion) {
+        return locomotion;
+    }
+
+    public Balise delete(Long balise_id) {
+        return null;
+    }
 }
