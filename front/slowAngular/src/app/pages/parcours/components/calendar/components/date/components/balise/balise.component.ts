3d7cef8be039c6e7db1760ef3d1547dbbc76845a
Adrien <amh.marceau@gmail.com>
2022-07-18 19:35:55 +0200

    front-pacours: :lipstick: ajout de CSS aux composants pour coller à la maquette

--- front/slowAngular/src/app/pages/parcours/components/calendar/components/date/components/balise/balise.component.ts
+++ front/slowAngular/src/app/pages/parcours/components/calendar/components/date/components/balise/balise.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {MatIconRegistry} from '@angular/material/icon';
+
+const BIN =
+  `
+  <svg width="32px" height="32px" viewBox="0 0 32 32" id="Outlined" xmlns="http://www.w3.org/2000/svg"><title/><g id="Fill"><rect height="12" width="2" x="15" y="12"/><rect height="12" width="2" x="19" y="12"/><rect height="12" width="2" x="11" y="12"/><path d="M20,6V5a3,3,0,0,0-3-3H15a3,3,0,0,0-3,3V6H4V8H6V27a3,3,0,0,0,3,3H23a3,3,0,0,0,3-3V8h2V6ZM14,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H14ZM24,27a1,1,0,0,1-1,1H9a1,1,0,0,1-1-1V8H24Z"/></g></svg>
+`;
 
 @Component({
   selector: 'app-balise',
@@ -7,7 +14,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BaliseComponent implements OnInit {
 
-  constructor() { }
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) { 
+    iconRegistry.addSvgIconLiteral('bin', sanitizer.bypassSecurityTrustHtml(BIN));
+  }
+
 
   ngOnInit(): void {
   }
