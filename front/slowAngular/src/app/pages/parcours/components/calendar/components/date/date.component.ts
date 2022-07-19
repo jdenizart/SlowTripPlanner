78a7a353054a9a7ccf61edf8363610a5afd9e089
Adrien <amh.marceau@gmail.com>
2022-07-19 19:13:24 +0200

    front-parcours: :lipstick: added CSS and HTML button classes

--- front/slowAngular/src/app/pages/parcours/components/calendar/components/date/date.component.ts
+++ front/slowAngular/src/app/pages/parcours/components/calendar/components/date/date.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {MatIconRegistry} from '@angular/material/icon';
+
+const ADD =
+  `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: rgb(0, 0, 0); --darkreader-inline-fill: #e8e6e3;" data-darkreader-inline-fill=""><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path></svg>
+`;
+
 
 @Component({
   selector: 'app-date',
@@ -6,4 +14,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./date.component.css']
 })
 export class DateComponent {
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) { 
+    iconRegistry.addSvgIconLiteral('add', sanitizer.bypassSecurityTrustHtml(ADD));
+  }
+
+
+  ngOnInit(): void {
+  }
 }
