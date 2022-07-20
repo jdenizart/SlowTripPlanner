df822da7a437669a1ea2205bc92ec2051ade3e4f
meiry11 <meirycisse11@gmail.com>
2022-07-20 16:57:59 +0200

    la configuration de la bdd

--- back/slowSpring/src/main/java/aston/slowspring/SlowSpringApplication.java
+++ back/slowSpring/src/main/java/aston/slowspring/SlowSpringApplication.java
@@ -2,8 +2,9 @@ package aston.slowspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SlowSpringApplication {
 
     public static void main(String[] args) {
---
+++ back/slowSpring/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO balise (nom_Balise) VALUES ('India');
+INSERT INTO balise (nom_Balise) VALUES ('Brazil');
+INSERT INTO balise (nom_Balise) VALUES ('USA');
+INSERT INTO balise (nom_Balise) VALUES ('Italy');
