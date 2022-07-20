5407f4ea62d6410aae85be73db6228d72e71686a
Adrien <amh.marceau@gmail.com>
2022-07-20 11:56:15 +0200

    front-parcours: refactor: datepicker format DD/MM/YYYY and added today date on first date by default

--- front/slowAngular/src/app/pages/parcours/components/calendar/components/date/date.component.ts
+++ front/slowAngular/src/app/pages/parcours/components/calendar/components/date/date.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 import {MatIconRegistry} from '@angular/material/icon';
+import {
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import 'moment/locale/ja';
+import 'moment/locale/fr';
 
 const ADD =
   `
@@ -11,12 +19,30 @@ const ADD =
 @Component({
   selector: 'app-date',
   templateUrl: './date.component.html',
-  styleUrls: ['./date.component.css']
+  styleUrls: ['./date.component.css'],
+  providers: [
+    // The locale would typically be provided on the root module of your application. We do it at
+    // the component level here, due to limitations of our example generation script.
+    {provide: MAT_DATE_LOCALE, useValue: 'fr'},
+
+    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
+    // `MatMomentDateModule` in your applications root module. We provide it at the component level
+    // here, due to limitations of our example generation script.
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ],
 })
 export class DateComponent {
   dates = new Array();
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) { 
+  TodayDate = new Date();
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer,private _adapter: DateAdapter<any>,
+    @Inject(MAT_DATE_LOCALE) private _locale: string,) { 
     iconRegistry.addSvgIconLiteral('add', sanitizer.bypassSecurityTrustHtml(ADD));
+    
   }
 
 
@@ -29,6 +55,10 @@ export class DateComponent {
 
   addvalue(){
     this.dates.push({value: ""});
+  }
+
+  getDateFormatString(): string {
+      return 'DD/MM/YYYY';
   }
   
 }
