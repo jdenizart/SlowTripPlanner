aab2c18325afab093cf82c800bf817d05c3baa3d
meiry11 <meirycisse11@gmail.com>
2022-07-24 22:14:15 +0200

    creation des services

--- back/slowSpring/src/main/java/aston/slowspring/SlowSpringApplication.java
+++ back/slowSpring/src/main/java/aston/slowspring/SlowSpringApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class SlowSpringApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SlowSpringApplication.class, args);
     }
 
