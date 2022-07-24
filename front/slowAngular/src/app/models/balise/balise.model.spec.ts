ecb98190a77952e77d5b3f6ae30dc80a4284a135
meiry11 <meirycisse11@gmail.com>
2022-07-24 22:18:39 +0200

    mise à jour des services

---
+++ front/slowAngular/src/app/models/balise/balise.model.spec.ts
@@ -0,0 +1,6 @@
+import { Balise } from "src/app/models/balise/balise.model"
+describe('Balise', () => {
+  it('should create an instance', () => {
+    expect(new Balise()).toBeTruthy();
+  });
+});
---
+++ front/slowAngular/src/app/models/balise/balise.model.ts
@@ -0,0 +1,10 @@
+export class Balise {
+    constructor(
+        public id: number,
+        public nomBalise: string,
+        public dateTime: Date,
+        public distance: any,
+        public estVerrouille: boolean,
+        
+    ){}
+}
