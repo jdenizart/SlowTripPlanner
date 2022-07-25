8cc5bb29add31c54644c6c4a6fe0add9e0efd113
meiry11 <meirycisse11@gmail.com>
2022-07-25 13:50:57 +0200

    mise à jour des services

--- back/slowSpring/src/main/java/aston/slowspring/SlowSpringApplication.java
+++ back/slowSpring/src/main/java/aston/slowspring/SlowSpringApplication.java
@@ -1,17 +1,20 @@
 package aston.slowspring;
 
 import aston.slowspring.entity.Balise;
+import aston.slowspring.entity.Profil;
 import aston.slowspring.repository.BaliseRepo;
+import aston.slowspring.repository.ProfilRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class SlowSpringApplication implements CommandLineRunner {
     @Autowired
     private BaliseRepo baliseRepo;
+    private ProfilRepo profilRepo;
     public static void main(String[] args) {
         SpringApplication.run(SlowSpringApplication.class, args);
     }
@@ -19,6 +22,8 @@ public class SlowSpringApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
     baliseRepo.save(new Balise("balise2",null ,2.0,true,null,null));
+    profilRepo.save(new Profil("meiry11","null","Respirez le courage, expirez la peur",null));
+
 
     baliseRepo.findAll().forEach(b->{
         System.out.println(b.toString());
